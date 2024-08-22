82f9ef02d403bb4cdcdff210dede07cc296b03bf
vanhvanh <vanhleg2301@gmail.com>
2024-08-22 16:29:28 +0700

    new

--- manaalbums/src/components/pages/home/PhotoCard.js
+++ manaalbums/src/components/pages/home/PhotoCard.js
@@ -49,45 +49,45 @@ export default function PhotoCard({
   };
 
   return (
-
-      <div className='photo-card'>
-        <Card>
-          <Card.Header>
-            <Row>
-              <Col xs={2}>
-                <Image
-                  width={50}
-                  height={50}
-                  src={"/assets/images/" + getAvatarByAlbumId(photo?.albumId)}
-                  alt={getUserNameByAlbumId(photo?.albumId)}
-                  className='rounded-circle'
-                />
-              </Col>
-              <Col xs={10}>
-                <div style={{ color: "black" }}>
-                  <strong>{getUserNameByAlbumId(photo?.albumId)}</strong>
-                </div>
-                <div style={{ color: "white" }}>
-                  {getAlbumDescription(photo?.albumId)}
-                </div>
-              </Col>
-            </Row>
-          </Card.Header>
-          <Card.Body>
-            <Link to={`photo/${photo?.id}`}>
-              <Card.Img
-                variant='top'
-                src={
-                  photo?.images?.thumbnail.startsWith("http")
-                    ? photo?.images?.thumbnail
-                    : "/assets/images/" +
-                      (photo?.images?.thumbnail || "logo192.png")
-                }
-                className='my-3'
-              />
-            </Link>
-          </Card.Body>
-          <Card.Footer>
+    <div className='photo-card'>
+      <Card.Header>
+        <Row>
+          <Col xs={2}>
+            <Image
+              width={50}
+              height={50}
+              src={"/assets/images/" + getAvatarByAlbumId(photo?.albumId)}
+              alt={getUserNameByAlbumId(photo?.albumId)}
+              className='rounded-circle'
+            />
+          </Col>
+          <Col xs={10}>
+            <div style={{ color: "black" }}>
+              <strong>{getUserNameByAlbumId(photo?.albumId)}</strong>
+            </div>
+            <div style={{ color: "black" }}>
+              {getAlbumDescription(photo?.albumId)}
+            </div>
+          </Col>
+        </Row>
+      </Card.Header>
+      <Card.Body>
+        <Link to={`photo/${photo?.id}`}>
+          <Card.Img
+            variant='top'
+            src={
+              photo?.images?.thumbnail.startsWith("http")
+                ? photo?.images?.thumbnail
+                : "/assets/images/" +
+                  (photo?.images?.thumbnail || "logo192.png")
+            }
+            className='my-3'
+          />
+        </Link>
+      </Card.Body>
+      <Card.Footer>
+        <Row>
+          <Col sx={6} sm={6} md={6}>
             <div className='action'>
               <Card.Text style={{ color: "black" }}>
                 <OverlayTrigger
@@ -114,6 +114,8 @@ export default function PhotoCard({
                 <i className='bi bi-share'></i>
               </Card.Text>
             </div>
+          </Col>
+          <Col sx={6} sm={6} md={6}>
             <div className='text-center d-flex'>
               <Button
                 onClick={() => handleLike(photo?.id)}
@@ -139,8 +141,9 @@ export default function PhotoCard({
                 <i className='bi bi-share'></i>
               </Button>
             </div>
-          </Card.Footer>
-        </Card>
-      </div>
+          </Col>
+        </Row>
+      </Card.Footer>
+    </div>
   );
 }
